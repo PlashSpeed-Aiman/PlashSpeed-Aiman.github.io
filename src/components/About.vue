d43ad9a426a73b1937d190a9debca6637a827987
aiman <maiman.ar@live.iium.edu.my>
2024-05-28 02:00:25 +0800

    UI Updates

--- src/components/About.vue
+++ src/components/About.vue
@@ -20,19 +20,19 @@
         <hr class="my-5"/>
         <h2 class="text-3xl font-bold mt-5 mb-2 text-center sm:text-justify">Skills</h2>
         <div class="my-5 grid grid-cols-1 gap-4 md:grid-cols-2">
-          <div class="bg-gray-100 p-5 rounded-md">
+          <div class=" bg-gradient-to-l from-gray-400 to-gray-200 p-5 rounded-md">
             <h3 class="text-2xl font-bold">Backend</h3>
             <p class="text-lg">Mostly on C# and .NET. But I can do NodeJS with Typescript, PHP with Laravel</p>
           </div>
-          <div class="bg-gray-100 p-5 rounded-md">
+          <div class="bg-gradient-to-l from-gray-400 to-gray-200 p-5 rounded-md">
             <h3 class="text-2xl font-bold">Frontend</h3>
             <p class="text-lg">I have experience with Vue (in fact this website is built with Vue)</p>
           </div>
-          <div class="bg-gray-100 p-5 rounded-md">
+          <div class="bg-gradient-to-l from-gray-400 to-gray-200 p-5 rounded-md">
             <h3 class="text-2xl font-bold">Databases</h3>
             <p class="text-lg">I have experience with SQL databases like SQL Server and SQLite. Currently learning how to make highly available systems</p>
           </div>
-          <div class="bg-gray-100 p-5 rounded-md">
+          <div class="bg-gradient-to-l from-gray-400 to-gray-200 p-5 rounded-md">
             <h3 class="text-2xl font-bold">DevOps</h3>
             <p class="text-lg">I have experience with Docker. Have some working knowledge on how to make docker images and get things up and running with docker compose.
               ToyyibPay (yes, the payment gateway company) even invited me for a sharing session on docker
