e54d26a5816f4db5ae293c00a47ab95108bc19f1
aiman <maiman.ar@live.iium.edu.my>
2024-05-30 00:52:21 +0800

    Web hash

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routes from './routes'
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
     })
 
